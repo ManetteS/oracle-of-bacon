981a7483edcec1f2ee9d07566eda8881d0a27572
ManetteS <Manette@172.17.3.70>
2018-02-21 09:31:14 +0100

    Added comment in MongoDbRepository to explain how to configure mongoDB

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/MongoDbRepository.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/repository/MongoDbRepository.java
@@ -8,11 +8,23 @@ import java.util.Optional;
 
 import static com.mongodb.client.model.Filters.eq;
 
+/*
+ * Dans un premier Shell, dans le bin de mongodb :
+ *   mongod --dbpath="<...>\oracle-of-bacon\imdb-data"
+ * Dans un second Shell, toujours dans le bin :
+ *   ./mongoimport -d actors -c actors --type csv --file "<...>\oracle-of-bacon\imdb-data\actors.csv" --headerline
+ *   mongo
+ * Dans le Shell mongo qui vient de s'ouvrir :
+ *   use actors
+ * Pour tester :
+ *   db.actors.find({"name:ID" : "Bacon, Kevin (I)"})
+ *     -> on recupere bien Kevin Bacon
+ */
 public class MongoDbRepository {
     private final String HOST = "localhost";
     private final String WORKSHOP = "workshop";
     private final String ACTORS = "actors";
-    private final String NAME = "name";
+    private final String NAME = "name:ID";
     private final MongoCollection<Document> actorCollection;
 
     public MongoDbRepository() {
